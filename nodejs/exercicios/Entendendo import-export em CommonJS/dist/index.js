342d6f7788d2e4860026b3bee35a58fc0630e379
Douglas <doug1306@gmail.com>
2022-02-22 21:54:07 -0300

    import-export

---
+++ nodejs/exercicios/Entendendo import-export em CommonJS/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const Matematica = require("./Matematica");
+let n1 = 10;
+let n2 = 2;
+console.log(`SOMA: ${Matematica.somar(n1, n2)}`);
+console.log(`SUBTRAÇÃO: ${Matematica.subtrair(n1, n2)}`);
+console.log(`MULTIPLICAÇÃO: ${Matematica.multiplicar(n1, n2)}`);
---
+++ nodejs/exercicios/Entendendo import-export em CommonJS/src/index.ts
@@ -0,0 +1,8 @@
+const Matematica = require("./Matematica");
+
+let n1: number = 10;
+let n2: number = 2;
+
+console.log(`SOMA: ${Matematica.somar(n1, n2)}`);
+console.log(`SUBTRAÇÃO: ${Matematica.subtrair(n1, n2)}`);
+console.log(`MULTIPLICAÇÃO: ${Matematica.multiplicar(n1, n2)}`);
