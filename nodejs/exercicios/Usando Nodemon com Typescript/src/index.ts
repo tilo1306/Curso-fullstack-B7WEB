4dc8a059e5bc77ccc3247d469efdedeeaab2e3a5
Douglas <doug1306@gmail.com>
2022-02-23 21:55:52 -0300

    mais exercicios

---
+++ nodejs/exercicios/Usando Nodemon com Typescript/src/index.ts
@@ -0,0 +1,7 @@
+import validator from "validator";
+let name: string = "Douglas";
+if (validator.isLowercase(name)) {
+  console.log(`A string ${name} é toda minúscula`);
+} else {
+  console.log(`A string ${name} NÃO é toda minúscula`);
+}
