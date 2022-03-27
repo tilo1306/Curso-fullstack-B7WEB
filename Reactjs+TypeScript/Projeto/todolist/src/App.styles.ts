94f0114d0e4de7c49277168c262ff92c0ef83d19
Douglas dos Santos Oliveira <doug1306@gmail.com>
2022-03-27 15:18:48 -0300

    styled do projeto

---
+++ Reactjs+TypeScript/Projeto/todolist/src/App.styles.ts
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: #17181f;
+  color: #797a81;
+  min-height: 100vh;
+
+  input {
+    width: 25px;
+    height: 25px;
+    margin-right: 5px;
+  }
+`;
+export const Area = styled.div`
+  margin: auto;
+  max-width: 980px;
+  padding: 10px;
+`;
+export const Header = styled.h1`
+  margin: 0;
+  padding: 0;
+  color: #fff;
+  text-align: center;
+  border-bottom: 1px solid #444;
+  padding-bottom: 20px;
+`;
+export const Section = styled.section`
+  display: flex;
+  background-color: #20212c;
+  padding: 10px;
+  border-radius: 10px;
+  align-items: center;
+  margin-bottom: 15px;
+`;
+export const Div = styled.div`
+  margin: auto 10px auto auto;
+`;
