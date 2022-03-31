d93134e5da1f2cf3d4e2b04a47ac523012bc9c68
Douglas dos Santos Oliveira <doug1306@gmail.com>
2022-03-31 20:39:10 -0300

    consulta2

---
+++ nodejs/exercicios/Módulo 3: Banco de dados SQL (mysql-postgresql)/Tipos de consulta 2/src/controllers/homeController.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+import { User } from "../models/Users";
+import { Product } from "../models/Product";
+import { Op } from "sequelize";
+
+export const home = async (req: Request, res: Response) => {
+  let users = await User.findAll({
+    where: {
+      age: {
+        [Op.between]: [40,70],
+        
+      }
+    }
+  });
+
+  let age: number = 90;
+  let showOld: boolean = false;
+
+  if (age > 50) {
+    showOld = true;
+  }
+
+  let list = Product.getAll();
+  let expensiveList = Product.getFromPriceAfter(12);
+
+  res.render("pages/home", {
+    name: "Bonieky",
+    lastName: "Lacerda",
+    showOld,
+    products: list,
+    expensives: expensiveList,
+    frasesDoDia: [],
+    users,
+  });
+};
