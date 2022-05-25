308795ae1ae873006cf1ee5445b35afe8f896438
Douglas dos Santos Oliveira <doug1306@gmail.com>
2022-05-25 20:21:07 -0300

    Entendendo o cors

--- nodejs/exercicios/Módulo 5: Criação de API/Entendendo o que é CORS/src/server.ts
+++ nodejs/exercicios/Módulo 5: Criação de API/Entendendo o que é CORS/src/server.ts
@@ -1,11 +1,14 @@
 import express, {Request, Response} from "express";
 import path from "path";
 import dotenv from "dotenv";
+import cors from "cors";
 import apiRoutes from "./routes/api";
 
 dotenv.config();
 
 const server = express();
+
+server.use(cors());
 
 server.use(express.static(path.join(__dirname, "../public")));
 server.use(express.urlencoded({extended: true}));
