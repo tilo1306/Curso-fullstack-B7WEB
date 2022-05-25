90aedec6e6f8d5dda78c90d9fb6a4da506c9b1cb
Douglas dos Santos Oliveira <doug1306@gmail.com>
2022-05-25 18:54:22 -0300

    aula projeto base

--- nodejs/exercicios/Módulo 3: Banco de dados SQL (mysql-postgresql)/Projeto base para seguir as aulas/src/models/Product.ts
+++ nodejs/exercicios/Módulo 3: Banco de dados SQL (mysql-postgresql)/Projeto base para seguir as aulas/src/models/Product.ts
@@ -1,20 +1,21 @@
 type Product = {
-    title: string,
-    price: number
+  title: string;
+  price: number;
 };
 
 const data: Product[] = [
-    {title: 'Produto X', price: 10},
-    {title: 'Produto Y', price: 15},
-    {title: 'Produto W', price: 20},
-    {title: 'Produto G', price: 5}
+  {title: "Produto X", price: 10},
+  {title: "Produto Y", price: 15},
+  {title: "Produto W", price: 20},
+  {title: "Produto G", price: 5},
 ];
+console.log("Segundo round?");
 
 export const Product = {
-    getAll: (): Product[] => {
-        return data;
-    },
-    getFromPriceAfter: (price: number): Product[] => {
-        return data.filter(item => item.price >= price);
-    }
+  getAll: (): Product[] => {
+    return data;
+  },
+  getFromPriceAfter: (price: number): Product[] => {
+    return data.filter((item) => item.price >= price);
+  },
 };
