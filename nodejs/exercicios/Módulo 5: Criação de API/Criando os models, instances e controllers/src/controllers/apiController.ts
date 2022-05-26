ae7256a16fe5908ebc411a8e21ad79de2811fe9f
Douglas dos Santos Oliveira <doug1306@gmail.com>
2022-05-25 22:48:23 -0300

    criando models

---
+++ nodejs/exercicios/Módulo 5: Criação de API/Criando os models, instances e controllers/src/controllers/apiController.ts
@@ -0,0 +1,14 @@
+import {Request, Response} from "express";
+import {Phrase} from "../models/Phrase";
+export const ping = (req: Request, res: Response) => {
+  res.json({pong: true});
+};
+
+export const random = (req: Request, res: Response) => {
+  let nRand: number = Math.floor(Math.random() * 10);
+  res.json({number: nRand});
+};
+export const nome = (req: Request, res: Response) => {
+  let nome: string = req.params.nome;
+  res.json(`Ola ${nome}, Tudo bem!`);
+};
