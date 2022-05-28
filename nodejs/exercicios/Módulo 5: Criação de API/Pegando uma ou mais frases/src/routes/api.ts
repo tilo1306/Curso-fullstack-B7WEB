cda4b40847a5cc362c1d4c1262303481e2da9482
Douglas dos Santos Oliveira <doug1306@gmail.com>
2022-05-28 20:51:21 -0300

    Pegando uma ou mais frases

---
+++ nodejs/exercicios/Módulo 5: Criação de API/Pegando uma ou mais frases/src/routes/api.ts
@@ -0,0 +1,14 @@
+import {Router} from "express";
+import * as ApiController from "../controllers/apiController";
+
+const router = Router();
+
+router.get("/ping", ApiController.ping);
+router.get("/random", ApiController.random);
+router.get("/nome/:nome", ApiController.nome);
+
+router.post("/frases", ApiController.createPhase);
+router.get("/frases", ApiController.listPhrases);
+router.get("/frase/:id", ApiController.getPhrases);
+
+export default router;
