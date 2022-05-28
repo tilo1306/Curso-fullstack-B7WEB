6400bd521f958f9d679c1a4a9ecf143b4213790a
Douglas dos Santos Oliveira <doug1306@gmail.com>
2022-05-27 23:00:27 -0300

    criando uma frase

---
+++ nodejs/exercicios/Módulo 5: Criação de API/Criando uma frase nova/src/controllers/apiController.ts
@@ -0,0 +1,23 @@
+import {Request, Response} from "express";
+import {Phrase} from "../models/Phrase";
+export const ping = (req: Request, res: Response) => {
+  res.json({pong: true});
+};
+
+export const random = (req: Request, res: Response) => {
+  let nRand: number = Math.floor(Math.random() * 10);
+  res.json({number: nRand});
+};
+export const nome = (req: Request, res: Response) => {
+  let nome: string = req.params.nome;
+  res.json(`Ola ${nome}, Tudo bem!`);
+};
+
+export const createPhase = async (req: Request, res: Response) => {
+  let {author, txt} = req.body;
+  let newPhrase = await Phrase.create({
+    author,
+    txt,
+  });
+  res.json({id: newPhrase.id, author, txt});
+};
---
+++ nodejs/exercicios/Módulo 5: Criação de API/Criando uma frase nova/src/routes/api.ts
@@ -0,0 +1,12 @@
+import {Router} from "express";
+import * as ApiController from "../controllers/apiController";
+
+const router = Router();
+
+router.get("/ping", ApiController.ping);
+router.get("/random", ApiController.random);
+router.get("/nome/:nome", ApiController.nome);
+
+router.post("/frase", ApiController.createPhase);
+
+export default router;
