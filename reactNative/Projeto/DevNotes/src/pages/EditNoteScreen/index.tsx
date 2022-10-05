c776e6e80fd9f9114a1b1210be88b1e7b26ee687
Douglas dos Santos Oliveira <doug1306@gmail.com>
2022-10-05 14:12:54 -0300

    feat: add inputs EditNoteScreen

--- reactNative/Projeto/DevNotes/src/pages/EditNoteScreen/index.tsx
+++ reactNative/Projeto/DevNotes/src/pages/EditNoteScreen/index.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
-import {Container, Text} from './styles';
+import {Container, TitleInput, BodyInput} from './styles';
 
 export const EditNoteScreen = () => {
+  const [title, setTitle] = React.useState('');
+  const [body, setBody] = React.useState('');
+
   return (
     <Container>
-      <Text>Tela editar</Text>
+      <TitleInput
+        value={title}
+        onChangeText={t => setTitle(t)}
+        placeholder="Digite o título da anotação"
+        placeholderTextColor="#CCC"
+        autoFocus={true}
+      />
+      <BodyInput
+        value={body}
+        onChangeText={t => setBody(t)}
+        placeholder="Digite o corpo da anotação"
+        placeholderTextColor="#CCC"
+        multiline={true}
+        textAlignVertical="top"
+      />
     </Container>
   );
 };
