3d7d2101bbc401aa5bb3a8de6b3847e9fee4c7ba
Douglas dos Santos Oliveira <doug1306@gmail.com>
2022-10-07 19:26:11 -0300

    feat: config prettier

--- reactNative/Projeto/DevFit/.prettierrc.js
+++ reactNative/Projeto/DevFit/.prettierrc.js
@@ -1,7 +1,9 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
+  semi: true,
   bracketSpacing: false,
+  jsxBracketSameLine: true,
   singleQuote: true,
   trailingComma: 'all',
+  arrowParens: 'avoid',
+  printWitdh: 80,
 };
