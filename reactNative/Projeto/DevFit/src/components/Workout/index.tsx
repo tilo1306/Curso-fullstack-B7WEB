9a0238ff3ba9fa1bcb6d5f1a548a6425c2ac921f
Douglas dos Santos Oliveira <doug1306@gmail.com>
2022-10-26 21:32:05 -0300

    feat: workout Component

--- reactNative/Projeto/DevFit/src/components/Workout/index.tsx
+++ reactNative/Projeto/DevFit/src/components/Workout/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useMuscleImage} from '../UsemuscleImage';
 import {
   Container,
   WorkoutInfo,
@@ -7,18 +8,32 @@ import {
   MuscleScroll,
   WorkoutButton,
   WorkoutButtonImage,
+  MuscleGroup,
+  MuscleImage,
 } from './styled';
 
 export const Workout = props => {
+  const muscleGroups = [];
+  for (const key in props.data.exercises) {
+    if (!muscleGroups.includes(props.data.exercises[key].muscle)) {
+      muscleGroups.push(props.data.exercises[key].muscle);
+    }
+  }
   return (
     <Container>
       <WorkoutInfo>
         <WorkoutTitle>{props.data.name}</WorkoutTitle>
-        <MuscleScroll horizontal={true} />
+        <MuscleScroll horizontal={true}>
+          {muscleGroups.map((m, index) => {
+            <MuscleGroup key={index}>
+              <MuscleImage source={useMuscleImage(m)} />
+            </MuscleGroup>;
+          })}
+        </MuscleScroll>
       </WorkoutInfo>
       <WorkoutActions>
         <WorkoutButton>
-          <WorkoutButtonImage />
+          <WorkoutButtonImage source={require('../../assets/add.png')} />
         </WorkoutButton>
       </WorkoutActions>
     </Container>
--- reactNative/Projeto/DevFit/src/components/Workout/styled.ts
+++ reactNative/Projeto/DevFit/src/components/Workout/styled.ts
@@ -14,7 +14,34 @@ export const WorkoutTitle = styled.Text`
   font-size: 17px;
   margin: 10px;
 `;
-export const MuscleScroll = styled.ScrollView``;
-export const WorkoutActions = styled.View``;
-export const WorkoutButton = styled.TouchableHighlight``;
-export const WorkoutButtonImage = styled.Image``;
+export const MuscleScroll = styled.ScrollView`
+  margin: 10px;
+`;
+export const WorkoutActions = styled.View`
+  justify-content: center;
+`;
+export const WorkoutButton = styled.TouchableHighlight`
+  width: 25px;
+  height: 25px;
+  margin: 20px;
+  justify-content: center;
+  align-items: center;
+`;
+export const WorkoutButtonImage = styled.Image`
+  width: 25px;
+  height: 25px;
+`;
+
+export const MuscleGroup = styled.View`
+  width: 40px;
+  height: 40px;
+  background-color: #ffcc98;
+  border-radius: 5px;
+  margin-right: 5px;
+  justify-content: center;
+  align-items: center;
+`;
+export const MuscleImage = styled.Image`
+  width: 30px;
+  height: 30px;
+`;
