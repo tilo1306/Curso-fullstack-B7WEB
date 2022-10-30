9bfcf79f2a74ce954876d36c19b6092446cf6bfd
Douglas dos Santos Oliveira <doug1306@gmail.com>
2022-10-29 21:07:07 -0300

    feat: start tabBar

---
+++ reactNative/Projeto/DevFit/src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Container} from './styled';
+import {Text} from 'react-native';
+
+export const Home = () => {
+  return (
+    <Container>
+      <Text>Pagina Home</Text>
+    </Container>
+  );
+};
